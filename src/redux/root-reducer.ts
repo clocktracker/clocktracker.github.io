cd69829a0bb42efec61d339606fca9f098a11c7b
Jordan Last <jordan.michael.last@gmail.com>
2016-09-21 15:59:28 -0600

    calculate total time incorrectly

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -17,6 +17,36 @@ export const RootReducer = (state=InitialState, action) => {
             newState.timeEntries = action.timeEntries;
             return newState;
         }
+        case 'CALCULATE_TOTAL_TIME': {
+            const newState = Object.assign({}, state);
+            newState.totalTime = newState.timeEntries.reduce((prev, curr, index, array) => {
+
+                if (typeof curr === 'boolean') {
+                    return prev;
+                }
+
+                if (!array[index + 1]) {
+                    return prev;
+                }
+
+                if (curr.type === 'CLOCK IN') {
+                    return prev;
+                }
+
+                const currentTime = curr.time;
+                const nextTime = array[index + 1].time;
+
+                const currentTimeMilliseconds = currentTime.getMilliseconds();
+                const nextTimeMilliseconds = nextTime.getMilliseconds();
+
+                const differenceInMilliseconds = nextTimeMilliseconds - currentTimeMilliseconds;
+
+                console.log(differenceInMilliseconds);
+
+                return prev + differenceInMilliseconds;
+            }, 0) / 1000;
+            return newState;
+        }
         default: {
             return state;
         }
